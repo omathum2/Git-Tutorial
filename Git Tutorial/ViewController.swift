2b95592d8d0d6b5ebd3e2ed817d97d295dea8583
omathum2 <mossy.omathuna@gmail.com>
2019-07-09 13:16:14 +0100

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Mossy O Mahony on 09/07/2019.
+//  Copyright © 2019 Mossy O Mahony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
